269988d542c4eade4e921ac151f7a1d1b837aa02
solankiuday <67455701+solankiuday@users.noreply.github.com>
2020-10-01 08:47:47 +0530

    Update conf2.py

--- conf2.py
+++ conf2.py
@@ -1,3 +1,4 @@
+#configuration for bolt module pair
 API_KEY = "XXXX"        //Bolt Cloud
 API KeyDEVICE_ID = "BOLTXXXX"    //Device ID of the Bolt Module
 FRAME_SIZE = 10        //Frame Size for Z score analysis
